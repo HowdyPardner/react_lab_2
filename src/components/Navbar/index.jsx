c465f5495b3e60e2defbf0e1ed07faa048732fb2
Marco Bragado <mbragado@LABA.PSWAS.LOCAL>
2023-09-11 15:50:59 -0700

    First commit. Add All necessary files for assignment requirements

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,41 @@
+// ﻿
+
+// Submit Github link.
+// Create a new react project (npx create-react-app appname)
+// open the appname folder in vs code.
+// Complete one of the following prompts:
+
+// 1. Create a navbar (Easier)
+    // 1. make a Navbar component
+    // 2. make a NavbarButton component 3. import NavbarButton into Navbar
+    // 4. Make 4 NavbarButton in Navbar
+    // 5. import Navbar into App.js and make 1 Navbar
+
+
+// 2. Make a form (Medium)
+    // 1. Make a form component
+    // 2. make 3 inputs and 1 button in the form component
+    // 3. make 3 states in the form
+        // 1. username, setUsername
+        // 2. email, setEmail
+        // 3. password, setPassword
+        // 4. connect the input values to the states (value, e.target.value)
+        // 5. add onClick to the button with a function handleClick
+        // 6. handleClick should log the states.
+
+
+// 3. Create a sidebar with hide functionality (Medium)
+    // 1. make a Sidebar component
+    // 2. make a SidebarButton component
+    // 3. import SidebarButton into Sidebar
+    // 4. Make 4 SidebarButton in Sidebar
+    // 5. import Sidebar intoApp.js and make 1 Sidebar
+    // 6. add a button to App.js that will hide the sidebar
+    // 7. app a piece of state called hideSidebar, setHideSidebar
+    // 8. when button is clicked, flip the state and conditionally render (display / not display) the sidebar 
+    
+    
+//4. Fetch data and display (Hard)
+    // 1. Make a Users and User components
+    // 2. in Users a useEffect, fetch 'https://jsonplaceholder.typicode.com/users' (array of user objects)
+    // 3. map through the array and for each user create a User instance (component) using that specific user's data
